7c000709a0ba470b539d1536b1d12b43fe0a737a
havefunatcode <dlxogh439@gmail.com>
2023-04-11 22:58:15 +0900

    elapsed_time 측정

--- chapter04/exercise2.py
+++ chapter04/exercise2.py
@@ -1,11 +1,16 @@
+import time as times
 n = int(input())
 
 cnt = 0
 
+start_time = times.time()
 for time in range(0, n + 1):
     for minute in range(0, 60):
         for second in range(0, 60):
             if '3' in str(second) or '3' in str(minute) or '3' in str(time):
                 cnt += 1
 
-print(cnt)
+end_time = times.time()
+elapsed_time = end_time - start_time
+
+print(f"Elapsed time: {elapsed_time:.2f} seconds")
--- chapter04/exercise2_1.py
+++ chapter04/exercise2_1.py
@@ -1,12 +1,16 @@
+import time as times
 n = int(input())
 
 time_with_three = [3, 13, 23]
 cnt = 0
 
+start_time = times.time()
 for time in range(0, n+1):
     if time in time_with_three:
         cnt += 3600
     else:
         cnt += 1575
 
-print(cnt)
+end_time = times.time()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time:.2f} seconds")
