2cbc21a1a386178d5f7483c4a50cf9a074dec6be
havefunatcode <dlxogh439@gmail.com>
2023-04-19 22:07:20 +0900

    elapsed time

--- chapter04/exercise2.py
+++ chapter04/exercise2.py
@@ -1,11 +1,16 @@
+import time as times
 n = int(input())
 
 cnt = 0
 
+start_time = times.time()
 for time in range(0, n + 1):
     for minute in range(0, 60):
         for second in range(0, 60):
             if '3' in (str(time) + str(minute) + str(second)):
                 cnt += 1
 
-print(cnt)
+end_time = times.time()
+elapsed_time = end_time - start_time
+
+print(f"Elapsed time: {elapsed_time:.2f} seconds")
